9aa695c2dac653f7fe438d95a44fe8a97dc1e7f3
Blair Billings <billings.blair@principal.com>
2023-01-06 08:28:06 -0600

    adds first reducers

---
+++ store/slices/commentSlice.js
@@ -0,0 +1,37 @@
+import {createSlice} from "@reduxjs/toolkit";
+import {HYDRATE} from "next-redux-wrapper";
+
+const initialState = {
+    value: [
+        {
+            comment: 'Gojo looks nice. Excellent work amigo!',
+            username: 'Saitama'
+        },
+        {
+            comment: 'Catoru Sensei! Konnichiwa!',
+            username: 'Yuji'
+        }
+    ]
+};
+
+export const commentSlice = createSlice({
+    name: 'comments',
+    initialState,
+    reducers: {
+        addComment: (state, action) => {
+            state.value = [...state.value, action.payload];
+        }
+    },
+    extraReducers: {
+        [HYDRATE]: (state, action) => {
+            return {
+                ...state,
+                ...action.payload.comments
+            };
+        }
+    }
+});
+
+export const { addComment } = commentSlice.actions;
+export const selectComments = (state) => state.comments.value;
+export default commentSlice.reducer;
